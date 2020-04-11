5f1dd1e86085abf631be7c75657079341c9b7973
NoorF <NoorF@IS7215LT.tmmc.na.corp.toyota.com>
2020-04-11 15:31:47 -0400

    Fix null issue with searchItem

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,50 +4,52 @@ import { CommunicationService } from 'src/app/services/common/communication.serv
 import { UserSession } from '../models/model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  searchContent: string;
-  contents: any;
-  currentSession: UserSession;
-  rows: number;
+	searchContent: string;
+	contents: any;
+	currentSession: UserSession;
+	rows: number;
 
-  constructor(
-	// private contentSearchService: ContentsearchService,
-	private comService: CommunicationService
-  	) { 
+	constructor(
+		// private contentSearchService: ContentsearchService,
+		private comService: CommunicationService
+	) {
 		this.retrieveFromLocalStorage();
 	}
 
-  ngOnInit() {
-    this.comService.userSession.subscribe(session => {
-      this.currentSession = session;
-     // this.retrieveFromLocalStorage()
-   
-    })
-  }
+	ngOnInit() {
+		this.comService.userSession.subscribe(session => {
+			this.currentSession = session;
+			// this.retrieveFromLocalStorage()
 
-  retrieveFromLocalStorage() {
-    this.currentSession = JSON.parse(localStorage.getItem('usersession'));
-    this.contents = this.currentSession.searchItem;
-    if (this.contents !== undefined) {
-      this.rows = this.contents.length / 3;
-      if (this.rows % 3 > 0) {
-			this.rows++;
-		  }
-    }
-    
-  }
+		})
+	}
+
+	retrieveFromLocalStorage() {
+		this.currentSession = JSON.parse(localStorage.getItem('usersession'));
+		if (this.currentSession != null) {
+			this.contents = this.currentSession.searchItem;
+
+			if (this.contents !== undefined) {
+				this.rows = this.contents.length / 3;
+				if (this.rows % 3 > 0) {
+					this.rows++;
+				}
+			}
+		}
+	}
 
 
-  selectContent(contentId: any) {
-	  this.currentSession = new UserSession();
-	  this.currentSession.contentId = contentId;
-	  this.currentSession.nextScreen = '<app-lesson>';
-	  localStorage.setItem('usersession', JSON.stringify(this.currentSession));
-	  this.comService.changeScreen(this.currentSession);
-  } 
+	selectContent(contentId: any) {
+		this.currentSession = new UserSession();
+		this.currentSession.contentId = contentId;
+		this.currentSession.nextScreen = '<app-lesson>';
+		localStorage.setItem('usersession', JSON.stringify(this.currentSession));
+		this.comService.changeScreen(this.currentSession);
+	}
 }
